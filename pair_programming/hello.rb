032272b101bd37bbe507f9610bc1c00ada80a09b
Victor Goff <keeperotphones@gmail.com>
2016-02-11 16:49:57 -0500

    Start to do our first pair programming

---
+++ pair_programming/hello.rb
@@ -0,0 +1,27 @@
+# doctest: I can greet the world
+# >> hello
+# => "Hello World!"
+
+
+__END__
+
+Vim Orientation, quickly...
+
+  gg takes us to the top of the file
+  G takes us to the bottom.
+  w will move forward a word
+  e will take us to the end of the word
+  b takes us to the beginning of the word
+  d deletes something
+  dw for example will delete a word. mnemonic...
+  f find something to to the right on the line.
+  / will search the file forward
+  ? will search the file backward
+  F Shift is a modifier key, for example when we press a but want a capital a we press shift to modify it to be a capital, we do the same thing with vim commands.
+      Escape key is the same as control-[
+escape takes us to the "normal mode"
+    i to insert
+    I to insert at the beginning of the line
+    a to append
+    A to append at the end of the line
+
