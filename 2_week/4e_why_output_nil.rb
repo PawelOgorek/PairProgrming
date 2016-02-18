302c7b774ac8217d92ea2181d7fc5ab5db8708b7
PawelOgorek <Pawel.Ogorek@poczta.fm>
2016-02-18 16:10:36 +0100

    Update 4e_why_output_nil.rb

---
+++ 2_week/4e_why_output_nil.rb
@@ -0,0 +1,8 @@
+
+puts expand = defined?( expand ) ? expand : true
+
+So firstly we check condition:
+                         defined?( expand )
+Which return 
+            false # because expand is not defined
+So, backing to statement - expand has no value...its null
