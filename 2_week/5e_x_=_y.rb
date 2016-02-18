be73e61634bce717e6673a621d0d70c43409944b
PawelOgorek <Pawel.Ogorek@poczta.fm>
2016-02-18 16:15:08 +0100

    Update 5e_x_=_y.rb

--- 2_week/5e_x_=_y.rb
+++ 2_week/5e_x_=_y.rb
@@ -1,2 +1,14 @@
-x, y = y, x 
+x, y = y, x
 its very usefull value swapping idea beetwen two variables. I.e. in java we have to use third variable and do it in few steps
+
+SO:
+
+    x = 10
+    y = 20
+    x, y = y, x
+    puts x
+    puts y
+
+equal: (because now x = y and y = x)
+    20
+    10
